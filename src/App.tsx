ba7eb141b6ad8ea79175381c698187ba21924ddd
negar <negar.karimnejad@gmail.com>
2024-03-30 12:37:52 +0330

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Homepage from "./components/home/Homepage";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
