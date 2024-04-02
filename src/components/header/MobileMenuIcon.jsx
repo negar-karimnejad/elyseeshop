c3ebc5746b6c6e3edc586d51c6c23971e396a5dd
negar <negar.karimnejad@gmail.com>
2024-04-02 15:42:30 +0330

    clean NavDropdownList up

--- src/components/header/MobileMenuIcon.jsx
+++ src/components/header/MobileMenuIcon.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 function MobileMenuIcon({ isMenuOpen, toggleMenu }) {
   return (
--- src/components/header/MobileNavMenu.jsx
+++ src/components/header/MobileNavMenu.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
-import NavUl from "./NavUl";
+import { navMobileMenu } from "../../data/data";
 import NavMobileDropdownLink from "./NavMobileDropdownLink";
+import NavUl from "./NavUl";
 
 const linkStyles =
   "block px-7 font-yekanB py-2 border-b border-dashed border-white";
@@ -9,37 +10,13 @@ function MobileNavMenu() {
   return (
     <div className="lg:hidden" id="mobile-menu">
       <div className="bg-pink-100">
-        <NavUl title="مراقبت از پوست">
-          <NavMobileDropdownLink title="مراقبت پوست" to="" />
-          <NavMobileDropdownLink title="مراقبت از چشم" to="" />
-          <NavMobileDropdownLink title="پاک کننده های صورت" to="" />
-          <NavMobileDropdownLink title="ابزار جانبی پوست" to="" />
-        </NavUl>
-        <NavUl title="لوازم آرایشی">
-          <NavMobileDropdownLink title="آرایش صورت" to="" />
-          <NavMobileDropdownLink title="آرایش چشم" to="" />
-          <NavMobileDropdownLink title="آرایش ابرو" to="" />
-          <NavMobileDropdownLink title="آرایش لب" to="" />
-          <NavMobileDropdownLink title="آرایش ناخن" to="" />
-          <NavMobileDropdownLink title="ابزار آرایش" to="" />
-        </NavUl>
-        <NavUl title="مراقبت از مو">
-          <NavMobileDropdownLink title="لوازم رنگ مو" to="" />
-          <NavMobileDropdownLink title="بهداشت و مراقبت مو" to="" />
-          <NavMobileDropdownLink title="حالت دهنده مو" to="" />
-        </NavUl>
-        <NavUl title="محصولات بدن">
-          <NavMobileDropdownLink title="مراقبت از بدن" to="" />
-          <NavMobileDropdownLink title="دئودورانت و ضد تعریق" to="" />
-          <NavMobileDropdownLink title="اصلاح صورت و بدن" to="" />
-          <NavMobileDropdownLink title="بهداشت شخصی" to="" />
-        </NavUl>
-        <NavUl title="عطر و ادکلن">
-          <NavMobileDropdownLink title="عطر و ادکلن" to="" />
-          <NavMobileDropdownLink title="غلظت" to="" />
-          <NavMobileDropdownLink title="نوع رایحه" to="" />
-        </NavUl>
-
+        {navMobileMenu.map((menu) => (
+          <NavUl title={menu.id} key={menu.id}>
+            {menu.list.map((item) => (
+              <NavMobileDropdownLink title={item} to="" />
+            ))}
+          </NavUl>
+        ))}
         <Link to="" className={linkStyles}>
           برندها
         </Link>
