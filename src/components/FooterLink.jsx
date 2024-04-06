f854d246757803d5bf94cddd8d2538530ba04952
negar <negar.karimnejad@gmail.com>
2024-04-06 23:42:40 +0330

    create terms page

--- src/components/FooterLink.jsx
+++ src/components/FooterLink.jsx
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom';
 const FooterLink = ({ link }) => {
   return (
     <div>
-      <h2 className="font-vazirBold mb-3 uppercase text-stone-700 dark:text-white">
-        {link.heading}
-      </h2>
-      <ul className="flex flex-col gap-2 font-medium text-stone-600 dark:text-stone-400">
+      <Link
+        to={link.heading.to}
+        className="font-vazirBold uppercase text-stone-700 dark:text-white"
+      >
+        {link.heading.title}
+      </Link>
+      <ul className="mt-3 flex flex-col gap-2 font-medium text-stone-600 dark:text-stone-400">
         {link.links.map((item) => (
           <li key={item}>
             <Link
-              to=""
+              to={item.to}
               className="transition-all hover:text-stone-700 hover:underline  dark:text-stone-300 dark:hover:text-stone-100"
             >
-              {item}
+              {item.title}
             </Link>
           </li>
         ))}
