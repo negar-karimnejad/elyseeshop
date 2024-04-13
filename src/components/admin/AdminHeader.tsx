6242647badd574c2807df2604a02255b40297366
negar <negar.karimnejad@gmail.com>
2024-04-13 22:27:34 +0330

    coding

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -1,13 +1,18 @@
+import { IoClose } from 'react-icons/io5';
+import { SlMenu } from 'react-icons/sl';
 import AdminNavbar from './AdminNavbar';
 
 function AdminHeader() {
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-3 flex items-center justify-between border-b bg-stone-600 lg:col-span-2">
+      <div className="col-span-3 flex items-center justify-between border-b bg-stone-600 max-md:col-span-12 lg:col-span-2">
         <h2 className="p-4 text-3xl font-bold tracking-widest text-pink-500">
           الیزه
         </h2>
-        <div className="p-4 text-3xl text-white">x</div>
+        <div className="p-4 text-3xl text-white">
+          <SlMenu size={24}/>
+          <IoClose size={24}/>
+        </div>
       </div>
       <AdminNavbar />
     </div>
--- src/components/admin/AdminNavbar.tsx
+++ src/components/admin/AdminNavbar.tsx
@@ -1,6 +1,27 @@
+import { useState } from 'react';
+import { CgProfile } from 'react-icons/cg';
+import Input from '../Input';
+
 function AdminNavbar() {
+  const [search, setSearch] = useState('');
   return (
-    <div className="col-span-9 bg-stone-100 lg:col-span-10">AdminNavbar</div>
+    <div className="max-md:hidden col-span-9 bg-stone-100 lg:col-span-10">
+      <div className="flex items-center justify-between border-b border-stone-400 px-10 py-5">
+        <h2 className="font-vazirBold text-xl text-stone-700">پنل مدیریت</h2>
+        <div className="flex items-center gap-2">
+          <form action="">
+            <Input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              type="text"
+              className="p-1"
+              placeholder="جستجو"
+            />
+          </form>
+          <CgProfile size={27} className="text-stone-500" />
+        </div>
+      </div>
+    </div>
   );
 }
 
